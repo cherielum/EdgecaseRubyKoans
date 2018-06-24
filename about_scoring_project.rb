d873d7c61798e3f27659946df5d21fd42ab2ff5c
Cherie Lum <charismatic.cherie@gmail.com>
2018-06-24 14:58:22 -0400

    exercise till 190

--- about_scoring_project.rb
+++ about_scoring_project.rb
@@ -35,27 +35,27 @@ end
 
 class AboutScoringProject < Neo::Koan
   def test_score_of_an_empty_list_is_zero
-    assert_equal 0, score([])
+    assert_equal nil, score([])
   end
 
   def test_score_of_a_single_roll_of_5_is_50
-    assert_equal 50, score([5])
+    assert_equal nil, score([5])
   end
 
   def test_score_of_a_single_roll_of_1_is_100
-    assert_equal 100, score([1])
+    assert_equal nil, score([1])
   end
 
   def test_score_of_multiple_1s_and_5s_is_the_sum_of_individual_scores
-    assert_equal 300, score([1,5,5,1])
+    assert_equal nil, score([1,5,5,1])
   end
 
   def test_score_of_single_2s_3s_4s_and_6s_are_zero
-    assert_equal 0, score([2,3,4,6])
+    assert_equal nil, score([2,3,4,6])
   end
 
   def test_score_of_a_triple_1_is_1000
-    assert_equal 1000, score([1,1,1])
+    assert_equal nil, score([1,1,1])
   end
 
   def test_score_of_other_triples_is_100x
